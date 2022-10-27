acd78e28ea5342cf1cf39d6b690c70a4b260908b
lonebhen <ben.angmortey@gmail.com>
2022-10-27 12:09:46 +0000

    backend-1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'hng_stage1.wsgi'
--- hng_stage1/settings.py
+++ hng_stage1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)91#19yqff3&*-6)h*c9y=qjn)^+bavl_pkx6(#pefvcmn*h$y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
